3e4abaae0679a223d2e3833706f251b0a0acf98c
Antoni Bertran <antoni@tresipunt.com>
2014-07-25 00:20:11 +0200

    Spinner loading

--- scripts/launch.js
+++ scripts/launch.js
@@ -1,6 +1,6 @@
 function lti_consumer_launch(id, id_lti, resource_link_id_val, is_modal, is_in_comments, internal_id) {
   var form = jQuery('form#launch-' + id);
-    jQuery("#iframe-modal-"+id).contents().find('html').html("<i class='icon-spinner icon-spin icon-large'></i>");
+    jQuery("#iframe-modal-"+id).contents().find('html').html("<link rel='stylesheet' href='http://getbootstrap.com/dist/css/bootstrap.min.css' type='text/css' media='screen'><link rel='stylesheet' href='http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css' type='text/css' media='screen'>  <br><br><div class=\"col-xs-5 col-centered\"><div class=\"item\"><div class=\"content\"></div></div></div><div class='col-xs-2 col-centered'><div class='item'><div class='content'><h1><i class='icon-spinner icon-spin icon-large glyphicon-align-center'></i></h1></div></div></div><div class=\"col-xs-5 col-centered\"><div class=\"item\"><div class=\"content\"></div></div></div>");
     jQuery.post(
       ajaxurl,
       {action: 'lti_launch', id: id_lti,  resource_link_id: resource_link_id_val, internal_id: internal_id, is_in_comments: is_in_comments}
